47d63cde8e3572a9fa3ed5d53c2486aa17e1267a
Arbaz <arbazk1444@gmail.com>
2025-11-08 19:17:59 +0530

    change config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: "/swizzlefowl-blogs",
+  assetPrefix: "/swizzlefowl-blogs",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  }
   /* config options here */
 };
 
